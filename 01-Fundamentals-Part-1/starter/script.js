701c3a8a93fa2dcec8baf0bbbeee160c6abcdf80
Adam Jeffreson <adam.jeffreson@gmail.com>
2024-03-12 13:10:37 +0000

    End of Lesson Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,54 @@
+/*
 let js = "amazing";
 console.log((40 + 30 + 8 - 6) / 2);
 
 let firstName = "Jonas";
 let PI = 3.1415;
+
 console.log(firstName);
 console.log(PI);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "jonas";
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+let job1 = "programmer";
+let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+//------------------------------------------------------------------------------------------//
+true;
+// set variable to a BOOL
+let jsIsFun = true;
+
+console.log(true);
+console.log(typeof true);
+
+// console log confirms BOOL
+console.log(typeof jsIsFun);
+
+console.log(typeof 66);
+console.log(typeof "A string of characters");
+
+// variable is overwritten with STRING
+jsIsFun = "YES";
+// console log now confirms the value within the
+console.log(typeof jsIsFun);
+
+// define variable without a value
+let year;
+console.log(year);
+console.log(typeof year);
+
+// assing the variable a new value
+year = 1991;
+console.log(year);
+console.log(typeof year);
+*/
