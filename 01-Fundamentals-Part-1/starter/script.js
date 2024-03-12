3d9cc1af696c7dfdd46b0121dfd203c162b538f6
Adam Jeffreson <adam.jeffreson@gmail.com>
2024-03-12 15:33:47 +0000

    EoL: Operator Precedence and Challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -60,9 +60,8 @@ const birthYear = 1991;
 birthYear = 1992; // gives an error due to mutating a const
 
 //------------------------------------------------------------------------------------------//
-
-const now = 2024;
 // Math Operators
+const now = 2024;
 const ageJonas = now - 1988;
 const ageSarah = now - 1978;
 console.log(ageJonas, ageSarah);
@@ -92,4 +91,50 @@ console.log(now - 1991 > now - 2018);
 
 //------------------------------------------------------------------------------------------//
 
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y; // x and y with no value
+x = y = 25 - 10 - 5; // evaluated left to right // x = y = 10
+console.log(x, y); // 10 10
+
+const averageAge = ageJonas + ageSarah / 2; // data within parenthesis is evaluated first
+console.log(ageJonas, ageSarah, averageAge);
 */
+//------------------------------------------------------------------------------------------//
+
+//Coding Challenge 1
+/*CHALLENGE #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+
+Your task is to write some code to help them:
+
+Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+Log the value of BMIMark and BMIJohn to the console.
+
+BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+ Write your code below. Good luck! 🙂 */
+/*
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+const MarkHigherBMI = BMIMark > BMIJohn;
+
+console.log("Marks BMI =", BMIMark, "Johns BMI =", BMIJohn);
+console.log("Does Mark Have a Higher BMI than John? ", MarkHigherBMI);
+*/
+//------------------------------------------------------------------------------------------//
