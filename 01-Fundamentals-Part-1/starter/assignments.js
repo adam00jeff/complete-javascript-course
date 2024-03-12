a4c4bc6597d349972a3502dcb1e8836a5317bf1f
Adam Jeffreson <adam.jeffreson@gmail.com>
2024-03-12 11:13:04 +0000

    End of Lesson Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,13 @@
+/*
+Values and Variables
+Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+Log their values to the console.
+*/
+let country = "UK";
+let continent = "Europe";
+let population = 65000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,7 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is fine");
-
 console.log((40 + 30 + 8 - 6) / 2);
+
+let firstName = "Jonas";
+let PI = 3.1415;
+console.log(firstName);
+console.log(PI);
