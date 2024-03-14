cb6dc9b58708f54446ed83708ad5a052e2d7c482
Adam Jeffreson <adam.jeffreson@gmail.com>
2024-03-14 13:26:43 +0000

    Eol: Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -15,4 +15,28 @@ if (hasDriversLicense) {
 //----------------------------------------------------------------------------------------/
 /*
 //Functions
+
+function logger() {
+  //function body
+  console.log("My name is Jonas");
+}
+
+// calling the function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // input data will be defined when called
+  // console.log(apples, oranges); // function peramaters can be used as normal vars
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice; // the value the function will return, becoming the result
+}
+
+const appleJuice = fruitProcessor(5, 0); // saves the return of the function to a variable for logging to the console
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4); // reuses the function with new values set
+console.log(appleOrangeJuice);
+
 */
+//----------------------------------------------------------------------------------------/
