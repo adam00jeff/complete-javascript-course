a7547705f0e082678801ea44b5f53bf43f927764
Adam Jeffreson <adam.jeffreson@gmail.com>
2024-03-14 11:46:25 +0000

    JavaScript Fundamentals Part 2 - Eol: Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// strict mode
+// all code from now onwards should have strict mode active
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hadDriverLicence = true; // typo in variable to show strict mode, if strict mode is active, an error is shown as an undefined variable
+if (hasDriversLicense) {
+  console.log(`I can drive`); // without strict mode this shows no errors but also no results
+}
+*/
+//----------------------------------------------------------------------------------------/
+/*
+//Functions
+*/
