b80209511a57fbaeb4afa5d813e8d874c0f11d38
Adam Jeffreson <adam.jeffreson@gmail.com>
2024-04-08 11:22:00 +0100

    Introduction to Arrays & Basic Array Operations

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -38,7 +38,7 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4); // reuses the function with new values set
 console.log(appleOrangeJuice);
 
-*/
+*/ /*
 //----------------------------------------------------------------------------------------/
 /*
 // Function Declarations vs. Expressions
@@ -60,7 +60,7 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
-*/
+*/ /*
 //----------------------------------------------------------------------------------------/
 /*
 
@@ -158,6 +158,7 @@ TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
 
 TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
  */
+/*
 // teams compete * 3 -> avg is calc
 // team 'wins' if their avg is > other teams avg * 2
 
@@ -167,23 +168,146 @@ const scoreAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 // create and assign variables for the avgs
 // test data 1
-const scoreDolphins = scoreAvg(44, 23, 71);
-const scoreKoalas = scoreAvg(65, 54, 49);
+let scoreDolphins = scoreAvg(44, 23, 71);
+let scoreKoalas = scoreAvg(65, 54, 49);
 
-// test data 2
-const scoreDolphins2 = scoreAvg(85, 54, 41);
-const scoreKoalas2 = scoreAvg(23, 34, 27);
+// test data 2 // commented out to use 'let' as below to reassign value
+//const scoreDolphins2 = scoreAvg(85, 54, 41);
+//const scoreKoalas2 = scoreAvg(23, 34, 27);
 
 // create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
 const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= avgKoalas * 2) {
-    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`); // call console.log inside the function
   } else if (avgKoalas >= avgDolphins * 2) {
     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
   } else {
     console.log(`No Team Wins...(${avgDolphins} vs. ${avgKoalas})`);
   }
 };
-console.log(checkWinner(scoreDolphins, scoreKoalas));
-console.log(checkWinner(scoreDolphins2, scoreKoalas2));
+//checkWinner(106, 505);
+//checkWinner(500, 100);
+//checkWinner(2, 3);
+checkWinner(scoreDolphins, scoreKoalas); // the console.log is inside the function, calling the function prints to console
+
+//test data 2
+scoreDolphins = scoreAvg(85, 54, 41);
+scoreKoalas = scoreAvg(23, 34, 27);
+checkWinner(scoreDolphins, scoreKoalas);
 */
+//----------------------------------------------------------------------------------------
+/*
+  Introduction to Arrays
+
+*/
+// seperate variables
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+// an array of variables
+const friends = ["Michael", "Steven", "Peter"]; // This is the 'Literal' Array syntax
+console.log(friends);
+
+//Array can be declared by name also
+const example = new Array(1111, 2222, 3333, 4444); // This is the Array function directly
+
+/* How the array looks in the console log:
+(3) ['Michael', 'Steven', 'Peter']
+0
+: 
+"Michael"
+1
+: 
+"Steven"
+2
+: 
+"Peter"
+length
+: 
+3
+[[Prototype]]
+: 
+Array(0)
+*/ /*
+console.log(friends[0]); // Arrays are zero based, so this is the first value in the Array
+console.log(friends[1]);
+
+console.log(friends.length); // number of elements in the array
+console.log(friends[friends.length - 1]); // finds the last element in the Array
+
+friends[2] = "Jay"; // friends is declared as a 'const', but only primitive values are immutable
+console.log(friends);
+
+//friends = ["Bob", "Alice"]; -- Causes an error, values in the array can be changed, but the whole array cannot be change at once as returns the below error
+// script.js:240 Uncaught TypeError: Assignment to constant variable.
+
+const jonas = ["Jonas", "Schmedtmann", 2037 - 1991]; // arrays can have calculations and multiple value types
+
+//Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+// calcAge(years); <- this will not work, as the function is not expecting an array, it is expecting a single value
+// the query above will throw a NaN error, as the array is 'not a number'
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]); // last value in array
+console.log(age1, age2, age3); //47 70 19
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]), // function calls can be used to construct arrays, as they return a value
+];
+console.log(ages);
+// /(3) [47, 70, 19]
+*/
+//----------------------------------------------------------------------------------------
+/*
+//Basic Array Operations (Methods)
+
+//Adding Elements
+const friends = ["Michael", "Steven", "Peter"];
+friends.push("Jay");
+console.log(friends); //(4) ['Michael', 'Steven', 'Peter', 'Jay']
+
+friends.unshift("John");
+console.log(friends); //(5) ['John', 'Michael', 'Steven', 'Peter', 'Jay']
+
+//Removing Elements
+friends.pop(); // removes the last element from the array
+console.log(friends); //(4) ['John', 'Michael', 'Steven', 'Peter']
+// pop returns the value removed from the array, these values can be logged as below
+const popped = friends.pop();
+console.log(popped); // Peter
+console.log(friends); //(3) ['John', 'Michael', 'Steven']
+
+friends.shift(); // will remove the first value in the array
+console.log(friends); // (2) ['Michael', 'Steven']
+
+// finding element info
+console.log(friends.indexOf("Steven")); // 1
+console.log(friends.indexOf("Bob")); // -1 <- returns -1 if the value is not found in the array
+
+// checking an array for an element
+console.log(friends.includes("Steven")); // True, value is found in the array
+console.log(friends.includes("Bob")); // False, the value is not found in the array
+
+friends.push(23); // adds 23 as a number to the array
+console.log(friends.includes("23")); // False, a string and a number are not found to be equal, due to strict equality
+console.log(friends.includes(23)); // True
+
+if (friends.includes("Peter")) {
+  console.log("You have a Peter");
+} else {
+  console.log("You have no Peters");
+}
+if (friends.includes("Steven")) {
+  console.log("You have a Steven");
+} else {
+  console.log("You have no Stevens");
+}
+*/
+//----------------------------------------------------------------------------------------
